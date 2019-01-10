e17337a21fb65de73e19dfa44ee930e2f87b58c8
zzzhim <3218566389@qq.com>
2019-01-10 14:40:42 +0800

    询盘页面完成

--- Modules/Ec/Frontend/src/views/Goods/Consulting/index.vue
+++ Modules/Ec/Frontend/src/views/Goods/Consulting/index.vue
@@ -9,19 +9,22 @@
         <main class="container consulting-main">
             <div class="consulting-main-body">
                 <div class="consulting-main-body-title">From "524155445@qq.com" <span>Edit</span></div>
+                <!-- 目标 -->
                 <div class="consulting-main-body-content">
                     <div class="consulting-main-body-content-label">To</div>
                     <div class="consulting-main-body-content-goods">
                         <span class="consulting-main-body-content-goods-title">wuhan sier international co., LTD</span>
                         <div>
-                            <v-img width="61" height="50" imgSrc="1"></v-img>
+                            <v-img width="61" height="50" imgSrc=""></v-img>
                         </div>
                     </div>
                 </div>
+                <!-- 主题 -->
                 <div class="consulting-main-body-content" style="marginTop: 24px;marginBottom: 32px;">
                     <div class="consulting-main-body-content-label">Subject</div>
                     <div class="consulting-main-body-content-subject">Inquiry about”Beidou GPS dual mode  vehicle navigation ”</div>
                 </div>
+                <!-- 采购 -->
                 <div class="consulting-main-body-content">
                     <div class="consulting-main-body-content-label" style="lineHeight: 34px;">Purchase Quantity</div>
                     <div class="consulting-main-body-content-purchase">
@@ -31,6 +34,7 @@
                         </Select>
                     </div>
                 </div>
+                <!-- 额外了解 -->
                 <div class="consulting-main-body-content" style="marginTop: 22px;marginBottom: 29px;">
                     <div class="consulting-main-body-content-label">Extra Request</div>
                     <div class="consulting-main-body-content-extra">
@@ -47,27 +51,34 @@
                         </CheckboxGroup>
                     </div>
                 </div>
+                <!-- 内容 -->
                 <div class="consulting-main-body-content">
                     <div class="consulting-main-body-content-label">Content</div>
-                    <textarea class="consulting-main-body-content-content" name="" id="" cols="30" rows="10">
-
-                    </textarea>
+                    <textarea class="consulting-main-body-content-content" name="" id="" cols="30" rows="10"></textarea>
+                    <div class="consulting-main-body-content-prompt">please enter the content for your inquiry</div>
                 </div>
-                <div class="consulting-main-body-content" style="marginTop: 34px;marginBottom: 17px;">
+                <!-- 上传文件 -->
+                <div class="consulting-main-body-content" style="marginTop: 34px;">
                     <div class="consulting-main-body-content-label"></div>
                     <div class="consulting-main-body-content-upload">
-                        <span><strong>Attach Files</strong> (ZIP/RAR/Word/Excel,Within20M)</span>
+                        <Upload action="//jsonplaceholder.typicode.com/posts/" style="width:85px">
+                            <label class="label">Attach Files</label>
+                        </Upload> 
+                        <span>
+                            <!-- 文件类型 -->
+                            (ZIP/RAR/Word/Excel,Within20M)
+                        </span>
                     </div>
                 </div>
                 <div class="consulting-main-body-content">
                     <div class="consulting-main-body-content-label"></div>
-                    <div>
-                        <span></span>
+                    <div class="consulting-main-body-content-fileName">
+                        <v-img width="13" height="13" :imgSrc="require('@/assets/img/fj.png')"></v-img>
                         <span>Pre-order document.rar</span>
-                        <span></span>
+                        <v-img width="16" height="16" :imgSrc="require('@/assets/img/qx.png')"></v-img>
                     </div>
                 </div>
-                <button class="consulting-main-body-content-btn">Send Inquiry</button>
+                <button class="consulting-main-body-content-btn" @click="onRouter">Send Inquiry</button>
             </div>
         </main>
     </div>
@@ -110,6 +121,11 @@
                 fruit: ['苹果']
             }
         },
+        methods: {
+            onRouter() {
+                this.$router.push("/goods/success")
+            }
+        },
         components: {
             "v-img": Img
         }
@@ -134,42 +150,43 @@
             margin: 0px auto;
 
             &-title {
+                .color(blackDark);
                 font-size: 16px;
                 line-height: 21px;
                 letter-spacing: 0px;
-                color: #333333;
                 padding-left: 99px;
                 margin-bottom: 24px;
 
                 & > span {
                     text-decoration: underline;
                     line-height: 21px;
-                    color: #f0883a;
+                    .color(Orange);
                 }
             }
 
             &-content {
                 .flex(flex-start, flex-start);
+                position: relative;
+
                 &-label {
                     .width(134px, 15px);
+                    .color(blackDark);
                     font-size: 16px;
                     line-height: 21px;
-                    letter-spacing: 0px;
-                    color: #333333;
                     margin-right: 10px;
                     text-align: right;
                 }
 
                 &-goods {
                     .width(449px, 144px);
-                    background-color: #f5f5f9;
+                    .bg-color(light);
                     padding: 15px 19px;
 
                     &-title {
+                        .color(blackLight);
                         width: 449px - 19px * 2;
                         font-size: 16px;
                         line-height: 1;
-                        color: #666666;
                         display: block;
                         .textHidden();
                         margin-bottom: 12px;
@@ -183,10 +200,10 @@
                 &-subject {
                     .width(449px, 47px);
                     .textHidden();
-                    background-color: #f5f5f9;
+                    .color(blackLight);
+                    .bg-color(light);
                     font-size: 16px;
                     line-height: 47px;
-                    color: #666666;
                     padding: 0px 14px;
                     text-align: center;
                 }
@@ -205,41 +222,58 @@
 
                 &-extra {
                     height: 20px;
-
-                    
                 }
 
                 &-content {
                     .width(449px, 111px);
                     border: solid 1px #dddddd;
+                    resize: none;
+                }
+                
+                &-prompt {
+                    .color(red);
+                    position: absolute;
+                    bottom: -16px;
+                    left: 144px;
+                    height: 13px;
+                    font-size: 13px;
                 }
 
                 &-upload {
+                    .flex();
+                    .color(gray);
                     font-size: 12px;
                     letter-spacing: 0px;
-                    color: #999999;
-
-                    & strong {
+                    & .label {
+                        .color(blackDark);
                         font-size: 16px;
-                        font-weight: normal;
-                        color: #333333;
                         cursor: pointer;
                         line-height: 1;
                     }
-                    & strong:hover {
-                        color: #f0883a;
+                    & .label:hover {
+                        .color(Orange);
+                    }
+                }
+
+                &-fileName {
+                    .flex(flex-start, center);
+                    height: 16px;
+
+                    & > span:nth-child(2) {
+                        margin-left: 9px;
+                        margin-right: 4px;
                     }
                 }
 
                 &-btn {
                     .width(133px, 39px);
-                    background-color: #f0883a;
+                    .color(white);
+                    .bg-color(Orange);
                     border: none;
                     font-size: 18px;
-                    color: #ffffff;
                     display: block;
                     margin: 0px auto;
-                    margin-top: 48px;
+                    margin-top: 45px;
                 }
             }
         }
