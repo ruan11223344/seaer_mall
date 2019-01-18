32664bcf347094fc1e2b260e0be3e792013cc5c4
zzzhim <3218566389@qq.com>
2019-01-18 11:35:36 +0800

    登录接口测试完成

--- Modules/Mall/Frontend/src/views/Login/index.vue
+++ Modules/Mall/Frontend/src/views/Login/index.vue
@@ -10,8 +10,9 @@
                 <div class="box-title">Account</div>
                 <!-- 用户id -->
                 <section class="box-useId">
-                    <input type="text" placeholder="Please enter your Email Address or Member ID." v-model="rulesFrom.userId" v-verify="rulesFrom.userId">
-                    <div class="box-useId-prompt" v-remind="rulesFrom.userId"></div>
+                    <input type="text" placeholder="Please enter your Email Address or Member ID." v-model="rulesFrom.userId" v-verify="rulesFrom.userId" @focus="bool=false">
+                    <div class="box-useId-prompt" v-remind="rulesFrom.userId">Please enter your Email Address or Member ID.</div>
+                    <div class="box-useId-prompt" v-show="bool">Please enter your Email Address or Member ID.</div>
                 </section>
                 <section class="box-title">
                     <span>Password:</span>
@@ -19,17 +20,18 @@
                 </section>
                 <!-- 用户密码 -->
                 <section class="box-useId">
-                    <input type="password" placeholder="Please enter your password." v-model="rulesFrom.password" v-verify="rulesFrom.password">
-                    <div class="box-useId-prompt" v-remind="rulesFrom.password">Incorrect Password.</div>
+                    <input type="password" placeholder="Please enter your password." v-model="rulesFrom.password" v-verify="rulesFrom.password" @focus="bool=false">
+                    <div class="box-useId-prompt" v-remind="rulesFrom.password">Please fill in the correct password</div>
+                    <div class="box-useId-prompt" v-show="bool">Please fill in the correct password</div>
                 </section>
                 <!-- 验证码 -->
-                <section class="box-useId box-code" style="marginTop: 40px;">
+                <section class="box-useId box-code" style="marginTop: 40px;" v-show="num >= 2">
                     <input type="text" placeholder="Please enter verification code.">
                     <div class="box-useId-prompt">Incorrect captcha.please enter the text in the image.</div>
-                    <div class="box-code-rex">
+                    <div class="box-code-rex" @click="getCodes">
                         <div>
                             <!-- 验证码图片 -->
-                           <img src="" alt="">
+                           <img :src="imgCode" alt="">
                         </div>
                         <Icon type="md-refresh" size="30"/>
                     </div>
@@ -43,14 +45,22 @@
 </template>
 
 <script>
+    // 引入公共获取数据方法
+    import getData from '@/utils/getData'
+    import Cookies from '@/utils/auth'
+
     export default {
         name: 'app',
         data() {
             return {
                 rulesFrom: {
                     userId: '',
-                    password: ''
-                }
+                    password: '',
+                    key: ''
+                },
+                imgCode: '',
+                num: 0,
+                bool: false
             }
         },
         verify: {
@@ -59,23 +69,56 @@
                 userId: [
                     'LoginUserId'
                 ],
-                password: [
-                    'LoginPassword'
-                ]
+                // password: [
+                //     'LoginPassword'
+                // ]
             }
         },
         methods: {
-            onSubmit() {
+            getCode: getData.getCode,
+            setCookies: Cookies.setCookies,
+            refreshCookies: Cookies.refreshCookies,
+            getCodes() {
+                const getCode = this.getCode()
+                getCode.then(({ code, data }) => {
+                    if(code == 200) {
+                        this.imgCode = data.img
+                        this.$set(this.rulesFrom, 'key', data.key)
+                    }
+                }).catch(err => console.log(err))
+            },
+            onSubmit() { // 提交
                 if(this.$verify.check()) {
-                    this.$Message.info('3秒后跳转到首页')
-
-                    setTimeout(() => {
-                        this.$router.push('/')
-                    }, 3000)
+                    this.upFrom()
                 }
             },
+            upFrom() {
+                this.$request({
+                    url: '/auth/get_access_token',
+                    method: 'post',
+                    data: {
+                        grant_type: 'password',
+                        client_id: 2,
+                        client_secret: 'LfmILOffY40xTlFbJT2Q0V8gWyyu99cwlElNPKrK',
+                        username: this.rulesFrom.userId,
+                        password: this.rulesFrom.password
+                    }
+                }).then((code, data) => {
+                    if(code == 200) {
+                        this.setCookies(data.access_token)
+                        this.refreshCookies(data.refresh_token)
+                        this.$route.push('/')
+                    }else {
+                        this.num++
+                        this.bool = true
+                    }
+                }).catch(err => {
+                    return false
+                })
+            }
         },
         mounted() {
+            this.getCodes()
         },
         components: {
         }
@@ -175,6 +218,11 @@
                         position: absolute;
                         top: 0px;
                         right: 0px;
+
+                        & > div:first-child {
+                            .width(172px, 60px);
+                            .flex(center, center);
+                        }
                     }
                 }
 
