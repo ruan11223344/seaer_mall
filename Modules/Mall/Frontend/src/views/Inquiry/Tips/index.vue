e639ea8ceaddcb09e8ec7cc6d3e4f2b023e3997c
zzzhim <3218566389@qq.com>
2019-02-14 16:06:17 +0800

    上传前审核静态界面完成

--- Modules/Mall/Frontend/src/views/Inquiry/Tips/index.vue
+++ Modules/Mall/Frontend/src/views/Inquiry/Tips/index.vue
@@ -1,15 +1,49 @@
 <template>
-    <div>
-        1
+    <div class="tips-main">
+        <div>
+            <v-img width="112" height="112" :imgSrc="require('@/assets/img/tips.png')"></v-img>
+        </div>
+        <div class="tips-main-text">You must complete your company information before displaying a new product. </div>
+        <div>
+            <button type="button" class="tips-main-btn">Click here</button>
+        </div>
     </div>
 </template>
 
 <script>
+    import Img from "@/components/Img"
+
     export default {
-        
+        components: {
+            "v-img": Img,
+        }
     }
 </script>
 
 <style lang="less" scoped>
+    @import url('../../../assets/css/index.less');
 
+    .tips-main {
+        .flex(center, center);
+        width: 944px;
+        height: 772px;
+        background-color: #ffffff;
+        padding: 21px 30px;
+        flex-direction: column;
+
+        &-text {
+            font-size: 20px;
+            color: #666666;
+            margin: 40px 0px;
+        }
+
+        &-btn {
+            width: 127px;
+	        height: 39px;
+            background-color: #f0883a;
+            font-size: 16px;
+            color: #ffffff;
+            border: none;
+        }
+    }
 </style>
