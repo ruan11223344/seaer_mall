9635f543b30878dd07e41a4ba1631acb119bc4c3
zzzhim <3218566389@qq.com>
2019-02-27 10:30:33 +0800

    获取公司信息

--- Modules/Mall/Frontend/src/views/Inquiry/Company/CompanyInfo/index.vue
+++ Modules/Mall/Frontend/src/views/Inquiry/Company/CompanyInfo/index.vue
@@ -2,74 +2,76 @@
     <div class="companyinfo">
         <v-title title="Company Info"></v-title>
 
-        <!-- 基本信息 -->
-        <section class="companyinfo-block">
-            <div class="companyinfo-block-title">Basic Info</div>
-            <article class="companyinfo-block-article">
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Business Type:</label>
-                    <span class="companyinfo-block-article-list-text">Trading Company</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Company Name:</label>
-                    <span class="companyinfo-block-article-list-text">Wuhan Sier International Co.,LTD.</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Company Name In China:</label>
-                    <span class="companyinfo-block-article-list-text">武汉思迩电子商务有限公司</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Country:</label>
-                    <span class="companyinfo-block-article-list-text">China</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Province/City:</label>
-                    <span class="companyinfo-block-article-list-text">HuBei WuHan</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Address:</label>
-                    <span class="companyinfo-block-article-list-text"> Room 605,Building A,No.777,Optical Valley three Road,East Lake High-tech Development Zone</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Telephone:</label>
-                    <span class="companyinfo-block-article-list-text">+86 027-65520870</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Website:</label>
-                    <span class="companyinfo-block-article-list-text">https://www.seaer.com.cn</span>
-                </div>
-            </article>
-        </section>
-        
-        <!-- 业务信息 -->
-        <section class="companyinfo-block">
-            <div class="companyinfo-block-title">Business Info</div>
-            <article class="companyinfo-block-article">
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Business Range:</label>
-                    <span class="companyinfo-block-article-list-text"> Agriculture & Food</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Main Products:</label>
-                    <span class="companyinfo-block-article-list-text">Garlic、Apple</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Company Profile: </label>
-                    <span class="companyinfo-block-article-list-text">We have ten years of local sales experience in Africa.It engages in cross-border e-commerce business activities such as S2B, B2B, B2C, and O2O. Its business products cover building materials, furniture, household products, automobile and motorcycle accessories, clothing, shoes and bags, 3C, IT digital, home appliances, computers, electronic products, beauty wigs, cosmetics, mother and baby, handicrafts, daily necessities, toys, stationery, sporting goods, sports outdoor products, jewelry, electric vehicles and their accessories, to achieve "buy the world, sell the world" to provide buyers and sellers in different countries with a one-stop butler service.</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Business License:</label>
-                    <span class="companyinfo-block-article-list-text">91420100MA4KXE9Q0A</span>
-                </div>
-                <div class="companyinfo-block-article-list">
-                    <label for="" class="companyinfo-block-article-list-label">Attachment:</label>
-                    <!-- <span class="companyinfo-block-article-list-text">https://www.seaer.com.cn</span> -->
-                    <v-img width="197" height="197" imgSrc=""></v-img>
-                </div>
-            </article>
-        </section>
+        <template v-if="formData != null">
+            <!-- 基本信息 -->
+            <section class="companyinfo-block">
+                <div class="companyinfo-block-title">Basic Info</div>
+                <article class="companyinfo-block-article">
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Business Type:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.basic_info.business_type }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Company Name:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.basic_info.company_name }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Company Name In China:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.basic_info.company_name_in_china }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Country:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.basic_info.country }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Province/City:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.basic_info["province/city"] }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Address:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.basic_info.address }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Telephone:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.basic_info.telephone }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Website:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.basic_info.website }}</span>
+                    </div>
+                </article>
+            </section>
+            
+            <!-- 业务信息 -->
+            <section class="companyinfo-block">
+                <div class="companyinfo-block-title">Business Info</div>
+                <article class="companyinfo-block-article">
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Business Range:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.business_info.business_range }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Main Products:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.business_info.main_products }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Company Profile: </label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.business_info.company_profile }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Business License:</label>
+                        <span class="companyinfo-block-article-list-text">{{ formData.business_info.business_license }}</span>
+                    </div>
+                    <div class="companyinfo-block-article-list">
+                        <label for="" class="companyinfo-block-article-list-label">Attachment:</label>
+                        <!-- <span class="companyinfo-block-article-list-text">https://www.seaer.com.cn</span> -->
+                        <img style="width: 197px; height: 197px; display: block;" :src="formData.business_info.business_license_url" />
+                    </div>
+                </article>
+            </section>
 
-        <button type="button" class="companyinfo-btn">Edit</button>
+            <button type="button" class="companyinfo-btn" @click="onEdit">Edit</button>
+        </template>
     </div>
 </template>
 
@@ -79,13 +81,26 @@
     import getData from "@/utils/getData.js"
 
     export default {
+        data() {
+            return {
+                formData: null
+            }
+        },
         methods: {
-            onGetCompanyInfo: getData.onGetCompanyInfo
+            onGetCompanyInfo: getData.onGetCompanyInfo,
+            onEdit() {
+                const formData = JSON.stringify(this.formData)
+                this.$router.push('/inquiryList/company/companyedit?formData=' + formData)
+            }
+        },
+        created() {
+            this.onGetCompanyInfo().then(res => {
+                this.formData = res
+                console.log(this.formData);
+            })
         },
         mounted() {
-            this.onGetCompanyInfo().then(res => {
-                console.log(res);
-            })
+            
         },
         components: {
             "v-title": Title,
