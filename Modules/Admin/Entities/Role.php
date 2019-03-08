d1c43b8f69bb190af06ad729f6f4ac823b15457d
jason <ruan4215@gmail.com>
2019-03-08 16:47:06 +0800

    1.fix bug

--- Modules/Admin/Entities/Role.php
+++ Modules/Admin/Entities/Role.php
@@ -3,4 +3,12 @@ namespace Modules\Admin\Entities;
 use Zizaco\Entrust\EntrustRole;
 class Role extends EntrustRole
 {
+    protected $table = 'roles';
+
+    protected $fillable = [
+        'id',
+        'name',
+        'display_name',
+        'description',
+    ];
 }
