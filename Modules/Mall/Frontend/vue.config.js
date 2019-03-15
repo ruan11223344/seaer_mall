d1ea2546c6bd516246c31762a6def0ed6b299bab
zzzhim <3218566389@qq.com>
2019-03-15 14:33:29 +0800

    修改webpack配置

--- Modules/Mall/Frontend/vue.config.js
+++ Modules/Mall/Frontend/vue.config.js
@@ -1,36 +1,34 @@
 const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  css: {
-    loaderOptions: { // 向 CSS 相关的 loader 传递选项
-      less: {
-        javascriptEnabled: true
-      }
-    }
-  },
-  // proxy API requests to Valet during development
-  // devServer: {
-  //   // proxy: 'http://laracon.test'
-  // },
-  publicPath: process.env.NODE_ENV === 'production' ? '/mall' : '/',
-  // output built static files to Laravel's public dir.
-  // note the "build" script in package.json needs to be modified as well.
-  // 输出目录
-  outputDir: '../public',
-  // 静态资源目录
-  // assetsDir: '../public',
-  configureWebpack: (config) => {
-    if (debug) { // 开发环境配置
-       config.devtool = 'cheap-module-eval-source-map'
-    } else { // 生产环境配置
-      config.devtool = 'cheap-module-source-map'
-    }
-  },
-  // 不生成map
-  // productionSourceMap: false,
-  // modify the location of the generated HTML file.
-  // make sure to do this only in production.
-  indexPath: process.env.NODE_ENV === 'production'
-    ? '../resources/views/index.blade.php'
-    : 'index.html'
+    css: {
+        loaderOptions: { // 向 CSS 相关的 loader 传递选项
+            less: {
+                javascriptEnabled: true
+            }
+        }
+    },
+    // proxy API requests to Valet during development
+    // devServer: {
+    //   // proxy: 'http://laracon.test'
+    // },
+    publicPath: process.env.NODE_ENV === 'production' ? '/mall' : '/',
+    // output built static files to Laravel's public dir.
+    // note the "build" script in package.json needs to be modified as well.
+    // 输出目录
+        // outputDir: '../public',
+    // 静态资源目录
+    assetsDir: '../public',
+    configureWebpack: (config) => {
+        if (debug) { // 开发环境配置
+        config.devtool = 'cheap-module-eval-source-map'
+        } else { // 生产环境配置
+        config.devtool = 'cheap-module-source-map'
+        }
+    },
+    // 不生成map
+    // productionSourceMap: false,
+    // modify the location of the generated HTML file.
+    // make sure to do this only in production.
+    indexPath: process.env.NODE_ENV === 'production' ? '../../resources/views/index.blade.php' : 'index.html'
 }
