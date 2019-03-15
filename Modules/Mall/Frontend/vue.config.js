84f942fb6e821c2ab8d2b857ee3f55409da8137a
zzzhim <3218566389@qq.com>
2019-03-15 11:14:55 +0800

    修改vue-config

--- Modules/Mall/Frontend/vue.config.js
+++ Modules/Mall/Frontend/vue.config.js
@@ -1,3 +1,5 @@
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
   css: {
     loaderOptions: { // 向 CSS 相关的 loader 传递选项
@@ -10,11 +12,22 @@ module.exports = {
   // devServer: {
   //   // proxy: 'http://laracon.test'
   // },
-  // baseUrl: process.env.NODE_ENV === 'production' ? '/mall' : '/',
+  baseUrl: process.env.NODE_ENV === 'production' ? '/mall' : '/',
   // output built static files to Laravel's public dir.
   // note the "build" script in package.json needs to be modified as well.
-  // outputDir: '../public',
-
+  // 输出目录
+  outputDir: '../public',
+  // 静态资源目录
+  assetsDir: '../public',
+  configureWebpack: (config) => {
+    if (debug) { // 开发环境配置
+       config.devtool = 'cheap-module-eval-source-map'
+    } else { // 生产环境配置
+      config.devtool = 'cheap-module-source-map'
+    }
+  },
+  // 不生成map
+  productionSourceMap: false,
   // modify the location of the generated HTML file.
   // make sure to do this only in production.
   indexPath: process.env.NODE_ENV === 'production'
