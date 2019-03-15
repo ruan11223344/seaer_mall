68162e318a82c490d1a2631b64820e36fcca6b4f
zzzhim <3218566389@qq.com>
2019-03-15 14:41:28 +0800

    修改vue-config配置

--- Modules/Mall/Frontend/vue.config.js
+++ Modules/Mall/Frontend/vue.config.js
@@ -9,26 +9,30 @@ module.exports = {
         }
     },
     // proxy API requests to Valet during development
-    // devServer: {
-    //   // proxy: 'http://laracon.test'
-    // },
+    devServer: {
+        // proxy: 'http://laracon.test'
+        overlay: {
+            warnings: false,
+            errors: true
+        }
+    },
     publicPath: process.env.NODE_ENV === 'production' ? '/mall' : '/',
     // output built static files to Laravel's public dir.
     // note the "build" script in package.json needs to be modified as well.
     // 输出目录
-        // outputDir: '../public',
+    outputDir: '../public',
     // 静态资源目录
-    assetsDir: '../public',
+    // assetsDir: '../../public',
     configureWebpack: (config) => {
         if (debug) { // 开发环境配置
-        config.devtool = 'cheap-module-eval-source-map'
+            config.devtool = 'cheap-module-eval-source-map'
         } else { // 生产环境配置
-        config.devtool = 'cheap-module-source-map'
+            config.devtool = 'cheap-module-source-map'
         }
     },
     // 不生成map
-    // productionSourceMap: false,
+    productionSourceMap: false,
     // modify the location of the generated HTML file.
     // make sure to do this only in production.
-    indexPath: process.env.NODE_ENV === 'production' ? '../../resources/views/index.blade.php' : 'index.html'
+    indexPath: process.env.NODE_ENV === 'production' ? '../Resources/views/index.blade.php' : 'index.html'
 }
