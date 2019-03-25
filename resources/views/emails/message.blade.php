0317f9b07c3cfacc3d41c6cc193af70a9da8d178
zzzhim <3218566389@qq.com>
2019-03-25 11:03:37 +0800

    更换邮箱模板

--- resources/views/emails/message.blade.php
+++ resources/views/emails/message.blade.php
@@ -5,108 +5,112 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
-
     <style>
         * {
-            padding: 0px;
             margin: 0px;
-            letter-spacing: 0px;
-            text-decoration: none;
-            line-height: 1;
-            font-family: ArialMT;
+            padding: 0px;
             font-weight: normal;
             font-stretch: normal;
+            font-family: ArialMT;
+            line-height: 1;
         }
-        .header-logo {
+        .main {
+            width: 100%;
+            height: 100vh;
+            background-color: #f5f5f9;
+            display: flex;
+        }
+        .block {
+            margin: 50px auto;
+        }
+        .header {
             width: 181px;
-            height: 53px;
-            background-color: azure;
+	        height: 53px;
         }
-
-        #main {
+        .header > img {
+            width: 100%;
+            height: 100%;
+            display: block;
+        }
+        .box {
             width: 765px;
-            height: 447px;
+            height: auto;
             background-color: #ffffff;
             border: solid 1px #dddddd;
-            margin-top: 14px;
+            margin-top: 15px;
+            padding: 0px 40px;
+            padding-bottom: 45px;
+            box-sizing: border-box;
         }
-        .main-hgroup {
-            margin-top: 42px;
-            margin-left: 55px;
-        }
-        .main-title {
+        .title {
             font-size: 16px;
-            line-height: 18.5px;
-            letter-spacing: 0px;
             color: #333333;
-        }
-        .main-title + h3 {
             font-size: 16px;
-            color: #999999;
-            margin-top: 5px;
+            margin-top: 47px;
+            margin-bottom: 26px;
+            text-align: center;
         }
-        .main-article {
-            margin: 31px 18px 0px 48px;
-        }
-        .main-article > p:first-child {
-            font-size: 14px;
+        .box > p {
+            margin: 10px 0px;
+            font-size: 16px;
             color: #666666;
-            text-indent: 2em;
-            line-height: 2;
-        }
-        .main-article > a:nth-child(2) {
-            font-size: 14px;
-            line-height: 2;
-            color: #f0883a;
-            word-break:break-all;
-            text-indent: 2em;
-        }
-        .main-article > p:last-child {
-            font-size: 14px;
-            color: #666666;
-            margin-top: 10px;
-            margin-bottom: 50px;
-        }
-        .main-btn {
-            width: 110px;
-            height: 40px;
-            background-color: #f0883a;
-            font-family: ArialMT;
-            font-size: 18px;
-            color: #ffffff;
-            cursor: pointer;
-            border: none;
-            margin: 0px auto;
-            display: block;
-            margin-top: 40px;
-        }
-
-        footer {
-            padding-left: 45px;
-        }
-        footer > div {
-            font-size: 14px;
-            color: #999999;
             line-height: 1.5;
         }
-    </style>
 
+        .box > p > span {
+            color: #f0883a;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        .btn {
+            width: 144px;
+            height: auto;
+            padding: 5px 15px;
+            background-color: #f0883a;
+            border: none;
+            color: #ffffff;
+            font-size: 14px;
+            line-height: 1.2;
+            color: #ffffff;
+            cursor: pointer;
+            display: block;
+            margin: 20px auto;
+            text-align: center;
+        }
+        
+        .footer {
+            margin-top: 25px;
+            font-size: 14px;
+            color: #999999;
+        }
+    </style>
 </head>
 <body>
-<header>
-    <div class="header-logo">
+    <div class="main">
+        <div class="block">
+            <div class="header">
+                <img src="./logo.png" alt="">
+            </div>
+            <div class="box">
+                <h1 class="title">Email Changed</h1>
+                <p>Notification: Business Message from <span>Miss.Nancy</span></p>
+                <p>TO: <span> WuHan Sier International CO,.LTD.</span></p>
+                <p>Dear: <span>Mr. Wangjun</span></p>
+                <p>Afriby.com would like to let you know that you have just received a new business message which is saved in the Inquiry Inbox.</p>
+                <a href="" class="btn">
+                        Reply Now <br>Go to Afriby.com
+                </a>
+                <div class="footer">
+                    Best regards
+                    <br>
+                    <br>
+                    Afriby.com
+                </div>
+            </div>
+        </div>
     </div>
-</header>
-<section id="main">
-    <hgroup class="main-hgroup">
-        <h1 class="main-title">You hava a message</h1>
-    </hgroup>
-    <article class="main-article">
-        {{ $messages }}
-    </article>
-    <footer>
-    这是一封测试邮件！
-    </footer>
-</section>
 </body>
 </html>
